58b3104c87e9a67ebbef6fe093e4560459eb9874
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-04-05 05:49:35 +0100

    Created button
    Created button with an onclick function

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,8 +1,8 @@
 import Header from './components/Header'
 function App() {
   return (
-    <div className="App">
-      <Header title="hello"/>
+    <div className="container">
+      <Header/>
     </div>
   );
 }
