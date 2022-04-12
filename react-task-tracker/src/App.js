c4a2e7d951bf0b9e5343535d1525aeeb3a7d3b33
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-04-12 03:23:57 +0100

    Added and 'AddTask' component
    Component Allows users to add new tasks to list.

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
 function App() {
   const [tasks , setTasks] = React.useState([
     {
@@ -29,6 +31,12 @@ function App() {
     }
   ])
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) +1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task)=> task.id !== id))
   }
@@ -40,6 +48,7 @@ function App() {
   return (
     <div className="container">
       <Header/>
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle = {toggleReminder}/> : "No Tasks To Show"}
     </div>
   );
